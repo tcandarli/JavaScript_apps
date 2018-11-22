b99a46aadab27b611c9b9378c65590620736bbca
tcandarli <tcandarli@gmail.com>
2018-11-22 22:08:20 +0100

    controller.js

---
+++ contactManager/js/controller.js
@@ -0,0 +1,15 @@
+/****************************
+ * UI
+ * 1. Click to add new contact and fill out the form.
+ * 2. Fill the form up with firstname, lastname, email, phone fields and click to save  button.
+ * 3. New contact information will send to contact list 
+ * 4. Selected contact shoudl be deleted
+ * 5. Search field can filter the contacts when clicked to search button.
+ * 
+ */
+
+ $(document).ready(() => {
+
+
+
+ });
